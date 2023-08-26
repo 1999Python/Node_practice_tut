c094a02eef94def574cb832a71cc8d2c862519d4
1999Python <raadiyaharend6@gmail.com>
2023-08-26 20:50:45 +0200

    completed

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const app = express();
-const PORT = 4000
-
-//Temp database
-
-
+const PORT = 4999
 //middleware
 app.use(express.json())
+app.use(express.static('public'))
+app.use(require('cors')())
+
+
+function mw (req, res, next){
+    console.log('Hit The Middleware')
+    next()
+}
+
+//Temp database
 const db = []
+
+//scheduler
+function cron(ms, fn){
+async function cb(){
+    clearTimeout(timeout)
+    await fn()
+    timeout = setTimeout(cb, ms)//ms millisecond //fn funct
+}
+let timeout = setTimeout(cb, ms)
+    return () => { }
+}
+function consoleDB(){
+    console.log('DB= ', db)
+}
+cron(1000, consoleDB)
+
+
 //GET POST PATCH PUT DELETE //get tryna get the the info from the server and post tryna send info to the sserver
 //
-app.get('/', (req, res) =>{
+app.get('/',(req, res) =>{
 
 console.log('raadi barbie')
 
@@ -31,7 +54,8 @@ app.post('/api/info', (req, res) =>{
     })
 
 //
-app.delete('/delete/:id', (req, res) =>{
+app.delete('/delete/:id', mw,(req, res) =>{
+    const {id, name} = req.params
    console.log('what pain would you like me to remove ') 
    res.sendStatus(200)
 })
@@ -40,7 +64,9 @@ app.delete('/delete/:id', (req, res) =>{
  
 //
 app.put('/api', (req, res) =>{
-
+    const { information } = req.body
+    console.log(information)
+    res.sendStatus(200)
 })
 
 
